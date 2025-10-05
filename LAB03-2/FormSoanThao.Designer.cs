590addb6250d63e06c3ec669a3688ad0afd61231
ChinXu236 <tphamphuongtrinh1402@gmail.com>
2025-10-05 17:15:13 +0700

    BaiTap_NOP

---
+++ LAB03-2/FormSoanThao.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace LAB03_2
+{
+    partial class FormSoanThao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hêThôngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taoVănBanMơiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mơTâpTinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lưuNôiDungVănBanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đinhDangToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 56);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(668, 212);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hêThôngToolStripMenuItem,
+            this.đinhDangToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(668, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hêThôngToolStripMenuItem
+            // 
+            this.hêThôngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.taoVănBanMơiToolStripMenuItem,
+            this.mơTâpTinToolStripMenuItem,
+            this.lưuNôiDungVănBanToolStripMenuItem,
+            this.thoatToolStripMenuItem});
+            this.hêThôngToolStripMenuItem.Name = "hêThôngToolStripMenuItem";
+            this.hêThôngToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.hêThôngToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hêThôngToolStripMenuItem.Text = "Hệ thống";
+            // 
+            // taoVănBanMơiToolStripMenuItem
+            // 
+            this.taoVănBanMơiToolStripMenuItem.Image = global::LAB03_2.Properties.Resources.file;
+            this.taoVănBanMơiToolStripMenuItem.Name = "taoVănBanMơiToolStripMenuItem";
+            this.taoVănBanMơiToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.taoVănBanMơiToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.taoVănBanMơiToolStripMenuItem.Text = "Tạo văn bản mới";
+            // 
+            // mơTâpTinToolStripMenuItem
+            // 
+            this.mơTâpTinToolStripMenuItem.Image = global::LAB03_2.Properties.Resources.folder;
+            this.mơTâpTinToolStripMenuItem.Name = "mơTâpTinToolStripMenuItem";
+            this.mơTâpTinToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.mơTâpTinToolStripMenuItem.Text = "Mở tập tin";
+            // 
+            // lưuNôiDungVănBanToolStripMenuItem
+            // 
+            this.lưuNôiDungVănBanToolStripMenuItem.Image = global::LAB03_2.Properties.Resources.diskette;
+            this.lưuNôiDungVănBanToolStripMenuItem.Name = "lưuNôiDungVănBanToolStripMenuItem";
+            this.lưuNôiDungVănBanToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.lưuNôiDungVănBanToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.lưuNôiDungVănBanToolStripMenuItem.Text = "Lưu nội dung văn bản";
+            // 
+            // thoatToolStripMenuItem
+            // 
+            this.thoatToolStripMenuItem.Name = "thoatToolStripMenuItem";
+            this.thoatToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
+            this.thoatToolStripMenuItem.Text = "Thoát";
+            // 
+            // đinhDangToolStripMenuItem
+            // 
+            this.đinhDangToolStripMenuItem.Name = "đinhDangToolStripMenuItem";
+            this.đinhDangToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.đinhDangToolStripMenuItem.Text = "Định dạng";
+            this.đinhDangToolStripMenuItem.Click += new System.EventHandler(this.ĐinhDangToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripComboBox1,
+            this.toolStripComboBox2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(668, 27);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::LAB03_2.Properties.Resources.file;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::LAB03_2.Properties.Resources.diskette;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(92, 27);
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(92, 27);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::LAB03_2.Properties.Resources.b;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = global::LAB03_2.Properties.Resources.italic;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton4.Text = "toolStripButton4";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = global::LAB03_2.Properties.Resources.underline;
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton5.Text = "toolStripButton5";
+            // 
+            // FormSoanThao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 360);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormSoanThao";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormSoanThao_Load_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hêThôngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem taoVănBanMơiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mơTâpTinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lưuNôiDungVănBanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem đinhDangToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+    }
+}
+
---
+++ LAB03-2/FormSoanThao.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LAB03_2
+{
+    public partial class FormSoanThao : Form
+    {
+        private string savedFilePath = null;
+
+        public FormSoanThao()
+        {
+            InitializeComponent();
+            this.Load += FormSoanThao_Load;
+            this.taoVănBanMơiToolStripMenuItem.Click += TaoVanBanMoi_Click;
+            this.mơTâpTinToolStripMenuItem.Click += MoTapTin_Click;
+            this.lưuNôiDungVănBanToolStripMenuItem.Click += LuuNoiDungVanBan_Click;
+            this.thoatToolStripMenuItem.Click += Thoat_Click;
+            // Button B/I/U: toolStripButton3/4/5
+            this.toolStripButton3.Click += BtnBold_Click;
+            this.toolStripButton4.Click += BtnItalic_Click;
+            this.toolStripButton5.Click += BtnUnderline_Click;
+            this.toolStripComboBox1.SelectedIndexChanged += ToolStripComboBox1_SelectedIndexChanged;
+            this.toolStripComboBox2.SelectedIndexChanged += ToolStripComboBox2_SelectedIndexChanged;
+        }
+
+        private void FormSoanThao_Load(object sender, EventArgs e)
+        {
+            // Load font
+            toolStripComboBox1.Items.Clear();
+            foreach (FontFamily fontFamily in new InstalledFontCollection().Families)
+            {
+                toolStripComboBox1.Items.Add(fontFamily.Name);
+            }
+            toolStripComboBox1.SelectedItem = "Tahoma";
+            // Load size
+            int[] sizeValues = new int[] { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            toolStripComboBox2.Items.Clear();
+            foreach (var size in sizeValues)
+                toolStripComboBox2.Items.Add(size);
+            toolStripComboBox2.SelectedItem = 14;
+            // Set default font
+            richTextBox1.Font = new Font("Tahoma", 14, FontStyle.Regular);
+        }
+
+        private void TaoVanBanMoi_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            toolStripComboBox1.SelectedItem = "Tahoma";
+            toolStripComboBox2.SelectedItem = 14;
+            richTextBox1.Font = new Font("Tahoma", 14, FontStyle.Regular);
+            savedFilePath = null;
+        }
+
+        private void MoTapTin_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|RichText files (*.rtf)|*.rtf";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                savedFilePath = selectedFileName;
+                try
+                {
+                    if (Path.GetExtension(selectedFileName).Equals(".txt", StringComparison.OrdinalIgnoreCase))
+                        richTextBox1.LoadFile(selectedFileName, RichTextBoxStreamType.PlainText);
+                    else
+                        richTextBox1.LoadFile(selectedFileName, RichTextBoxStreamType.RichText);
+                    MessageBox.Show("Tập tin đã được mở thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã xảy ra lỗi trong quá trình mở tập tin: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LuuNoiDungVanBan_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(savedFilePath))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "RichText files (*.rtf)|*.rtf";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    savedFilePath = saveFileDialog.FileName;
+                    richTextBox1.SaveFile(savedFilePath, RichTextBoxStreamType.RichText);
+                    MessageBox.Show("Lưu văn bản thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                richTextBox1.SaveFile(savedFilePath, RichTextBoxStreamType.RichText);
+                MessageBox.Show("Lưu văn bản thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Thoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnBold_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                FontStyle style = richTextBox1.SelectionFont.Style;
+                if (richTextBox1.SelectionFont.Bold)
+                    style &= ~FontStyle.Bold;
+                else
+                    style |= FontStyle.Bold;
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, style);
+            }
+        }
+        private void BtnItalic_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                FontStyle style = richTextBox1.SelectionFont.Style;
+                if (richTextBox1.SelectionFont.Italic)
+                    style &= ~FontStyle.Italic;
+                else
+                    style |= FontStyle.Italic;
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, style);
+            }
+        }
+        private void BtnUnderline_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                FontStyle style = richTextBox1.SelectionFont.Style;
+                if (richTextBox1.SelectionFont.Underline)
+                    style &= ~FontStyle.Underline;
+                else
+                    style |= FontStyle.Underline;
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, style);
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            // Tạo văn bản mới: xóa nội dung, đặt lại font và size mặc định
+            richTextBox1.Clear();
+            toolStripComboBox1.SelectedItem = "Tahoma";
+            toolStripComboBox2.SelectedItem = 14;
+            richTextBox1.Font = new Font("Tahoma", 14, FontStyle.Regular);
+            savedFilePath = null;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            // Mở tập tin: hộp thoại chọn file txt/rtf
+            richTextBox1.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|RichText files (*.rtf)|*.rtf";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                savedFilePath = selectedFileName;
+                try
+                {
+                    if (Path.GetExtension(selectedFileName).Equals(".txt", StringComparison.OrdinalIgnoreCase))
+                        richTextBox1.LoadFile(selectedFileName, RichTextBoxStreamType.PlainText);
+                    else
+                        richTextBox1.LoadFile(selectedFileName, RichTextBoxStreamType.RichText);
+                    MessageBox.Show("Tập tin đã được mở thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã xảy ra lỗi trong quá trình mở tập tin: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Button3: Tô đậm
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                FontStyle newFontStyle;
+                // Toggle Bold
+                if (currentFont.Bold)
+                    newFontStyle = currentFont.Style & ~FontStyle.Bold;
+                else
+                    newFontStyle = currentFont.Style | FontStyle.Bold;
+
+                richTextBox1.SelectionFont = new Font(currentFont, newFontStyle);
+            }
+        }
+
+        // Button4: In nghiêng
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                FontStyle newFontStyle;
+                // Toggle Italic
+                if (currentFont.Italic)
+                    newFontStyle = currentFont.Style & ~FontStyle.Italic;
+                else
+                    newFontStyle = currentFont.Style | FontStyle.Italic;
+
+                richTextBox1.SelectionFont = new Font(currentFont, newFontStyle);
+            }
+        }
+
+        // Button5: Gạch chân
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                FontStyle newFontStyle;
+                // Toggle Underline
+                if (currentFont.Underline)
+                    newFontStyle = currentFont.Style & ~FontStyle.Underline;
+                else
+                    newFontStyle = currentFont.Style | FontStyle.Underline;
+
+                richTextBox1.SelectionFont = new Font(currentFont, newFontStyle);
+            }
+        }
+
+        private void ToolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFontOrSize();
+        }
+
+        private void ToolStripComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFontOrSize();
+        }
+
+        private void ChangeFontOrSize()
+        {
+            string fontName = toolStripComboBox1.SelectedItem?.ToString() ?? richTextBox1.Font.FontFamily.Name;
+            float fontSize = Convert.ToSingle(toolStripComboBox2.SelectedItem ?? richTextBox1.Font.Size);
+            FontStyle style = richTextBox1.SelectionFont?.Style ?? FontStyle.Regular;
+            richTextBox1.SelectionFont = new Font(fontName, fontSize, style);
+        }
+
+        private void FormSoanThao_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void ĐinhDangToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (FontDialog fontDialog = new FontDialog())
+            {
+                // nếu có text được chọn thì đặt font mặc định cho dialog
+                if (richTextBox1.SelectionFont != null)
+                    fontDialog.Font = richTextBox1.SelectionFont;
+                else
+                    fontDialog.Font = richTextBox1.Font;
+
+                if (fontDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (richTextBox1.SelectionLength > 0)
+                    {
+                        // áp dụng cho đoạn text được chọn
+                        richTextBox1.SelectionFont = fontDialog.Font;
+                    }
+                    else
+                    {
+                        // áp dụng cho chữ sẽ gõ tiếp theo
+                        richTextBox1.SelectionFont = fontDialog.Font;
+                    }
+
+                    // cập nhật lại combobox font + size
+                    toolStripComboBox1.SelectedItem = fontDialog.Font.FontFamily.Name;
+                    toolStripComboBox2.SelectedItem = (int)fontDialog.Font.Size;
+                }
+            }
+        }
+    }
+}
